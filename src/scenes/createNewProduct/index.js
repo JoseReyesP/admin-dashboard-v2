14ff6e1decef77de4d8bfa1608981d8e746588bc
Mrk197 <mirkaalamilla97@gmail.com>
2024-01-12 14:17:42 -0600

    add new product form

--- src/scenes/createNewProduct/index.js
+++ src/scenes/createNewProduct/index.js
@@ -1,7 +1,38 @@
+import { Box, Container, Grid, Stack } from "@mui/material";
+import FormNewProduct from "components/FormNewProduct";
+import Header from "components/Header";
 import React from "react";
 
+
 const CreateNewProduct = () => {
-  return <div>CreateNewProduct</div>;
+  return <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth="md">
+        <Stack spacing={3}>
+          <div>
+            <Header title="New Product"/>
+          </div>
+          <div>
+            <Grid
+              container
+            >
+              <Grid
+                xs={10}
+                md={10}
+                lg={12}
+              >
+              <FormNewProduct/>
+              </Grid>
+            </Grid>
+          </div>
+        </Stack>
+      </Container>
+    </Box>;
 };
 
 export default CreateNewProduct;
